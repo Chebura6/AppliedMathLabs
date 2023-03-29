e8ebbe16d554e2af2695308f6d7d779d46844e13
Artem <parfenovartemartem@yandex.ru>
2023-03-29 17:41:40 +0300

    feat: Task 1

--- Lab1/Task1.py
+++ Lab1/Task1.py
@@ -1,35 +1,18 @@
 import math
 import numpy as np
 
-a = 0
-b = 10
-h = 0.1
-
-n = round((b - a) / h)
-
-
-def right(f, x):
-    global h
+def right(f, x, h):
     return (f(x + h) - f(x)) / h
 
 
-def left(f, x):
-    global h
+def left(f, x, h):
     return (f(x) - f(x - h)) / h
 
 
-def central(f, x):
-    global a
-    global h
+def central(f, x, h, a):
     i = x / h
     x_l = a + h * (i - 1)
     y_l = f(x_l)
     x_r = a + h * (i + 1)
     y_r = f(x_r)
     return (y_r - y_l) / (2 * h)
-
-#
-# x0 = 5.625
-# print(right(f1, x0))
-# print(left(f1, x0))
-# print(central(f1, x0))
